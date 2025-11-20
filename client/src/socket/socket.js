e07d74055561603d12aac708aef89a42fcf604c4
MahwayiRuth <ruthmahwayi@gmail.com>
2025-11-20 22:20:22 +0200

    Update socket.js

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -2,7 +2,7 @@
 
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000';
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'https://my-chat-app-mahwayiruth.onrender.com';
 
 class SocketService {
   constructor() {
